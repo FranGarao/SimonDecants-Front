12df66953a5611d3067b7484717e10988d09df2c
Leonel Adrian Bruzzese <lbruzzese@HCJSM.local>
2024-03-19 13:35:11 -0300

    se estilo el login desktop y responsive y se aumento la fuente del logo de la pagina

--- src/app/pages/main/components/header/header.component.ts
+++ src/app/pages/main/components/header/header.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public showLogin: boolean = false;
+
+  openLogin() {
+    this.showLogin = !this.showLogin;
+  }
 }
+
